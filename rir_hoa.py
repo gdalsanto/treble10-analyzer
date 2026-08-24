ebc22e1ddbdcbbb351c670ab454a4fbf0f4df921
Gloria Dal Santo <gloria.dalsanto@outlook.it>
2026-08-24 12:43:16 +0300

    Skip decoder

--- rir_hoa.py
+++ rir_hoa.py
@@ -36,8 +36,6 @@ def main(args):
     output_dir = args.output_dir
     ds = load_dataset(path_to_dataset, streaming=True, split=split, cache_dir="/scratch/work/dalsag1/.cache/huggingface/datasets")
 
-    ds = ds.cast_column("audio", Audio(decode=True))
-
     # collect all examples, grouped by (room, source) in a single streaming pass
     # for each rir save the rir, the mic position and the source position in a dictionary
     def new_subset():
